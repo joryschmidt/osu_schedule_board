87fd40721ed53b8b216571c030dd17d4d21cf1f0
Jory Schmidt <joryschmidt1988@gmail.com>
2019-07-09 20:58:03 +0000

    write report generating method

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -24,9 +24,29 @@ export class AppComponent {
   
   generateReport() {
     if (this.date) {
+      // if date is selected, generate report
       this.data.getFlightsByDate(this.date).subscribe(response => {
-        window.open(`${response}`);
+        var date = response[0].date;
+        var items = [];
+        var wind = window.open('', date, 'width=1000, height=1000');
+        
+        // create lines of html from the response
+        for (let flight in response) {
+          let tail = response[flight].tail || '';
+          let time = response[flight].time ? `| ${ response[flight].time }` : '';
+          let remarks = response[flight].remarks ? `| ${ response[flight].remarks }` : '';
+          
+          let item = `<p>${tail} ${time} ${remarks}</p>`;
+          items.push(item);
+        }
+        
+        // write lines to new opened window
+        wind.document.write(`<h1>${date}</h1>`);
+        for (var i=0, end=items.length; i<end; i++) {
+          wind.document.write(items[i]);
+        }
       });
+      
     } else {
       console.log('Please select a date');
     }
